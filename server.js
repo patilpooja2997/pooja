16bf8556515d72f6f7ab54581936e494896710b7
Pooja Patil <pppatil9763@gmail.com>
2019-11-14 15:13:50 +0530

    added server file

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+
+app.get('/', (request, response) => {
+    response.send('welcome to my application')
+})
+
+app.post('/', (request, response) => {
+    response.send('welcome to her application')
+})
+
+
+app.listen(4000, () => {
+    console.log(`Server started on port 4000`);
+});
